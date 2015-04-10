61b5f03d8b7eb470a8778d951bfe7a9c68ecc4b1
Ben Rhodes <b.rhodes@techsmith.com>
2015-04-10 14:59:30 -0400

    Refactored rectangle object

--- src/rectangle.js
+++ src/rectangle.js
@@ -1,10 +1,50 @@
 'use strict';
 
 module.exports = function() {
-   return {
-      x: 0,
-      y: 0,
-      width: 0,
-      height: 0
-   }
+   var _x;
+   var _y;
+   var _width;
+   var _height;
+
+   _x = 0;
+   _y = 0;
+   _width = 0;
+   _height = 0;
+
+   return Object.defineProperties({},
+      {
+         x: {
+            get: function() {
+               return _x;
+            },
+            set: function(value) {
+               _x = value;
+            }
+         },
+         y: {
+            get: function() {
+               return _y;
+            },
+            set: function(value) {
+               _y = value;
+            }
+         },
+         width: {
+            get: function() {
+               return _width;
+            },
+            set: function(value) {
+               _width = value;
+            }
+         },
+         height: {
+            get: function() {
+               return _height;
+            },
+            set: function(value) {
+               _height = value;
+            }
+         }
+      }
+   );
 };
