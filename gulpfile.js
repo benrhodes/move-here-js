46b606608bf870f659e0437f08784b1ad0722c47
Ben Rhodes <b.rhodes@techsmith.com>
2015-08-18 16:02:31 -0400

    Moved gulp karma task to make it work

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,18 @@ var gulp = require('./gulp')([
    'jshint',
    'jsdoc',
    'cleandocs',
-   'karma',
    'babel'
 ]);
 
-gulp.task('build', ['babel']);
+var Server = require('karma').Server;
+var karma = function (done) {
+   new Server({
+      configFile: __dirname + '/karma.conf.js',
+      singleRun: true
+   }, done).start();
+};
+
+gulp.task('karma', karma);
+gulp.task('build', ['karma', 'babel']);
 gulp.task('default', ['build']);
 gulp.task('docs', ['cleandocs', 'jsdoc']); // does not work with ES6 syntax
