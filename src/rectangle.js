08d09aeb00a1ed4ccb2f25ad6ff5dfddd229affa
Ben Rhodes <b.rhodes@techsmith.com>
2015-08-24 15:25:29 -0400

    cached percentage of width and height for rectangles

--- src/rectangle.js
+++ src/rectangle.js
@@ -4,6 +4,10 @@ export default class Rectangle {
       this._y = y;
       this._width = width;
       this._height = height;
+      this._halfWidth = width / 2;
+      this._halfHeight = height / 2;
+      this._eighthWidth = width / 8;
+      this._eighthHeight = height / 8;
    }
 
    set x(value){
@@ -24,6 +28,8 @@ export default class Rectangle {
 
    set width(value){
       this._width = value;
+      this._halfWidth = value / 2;
+      this._eighthWidth = value / 8;
    }
 
    get width(){
@@ -32,9 +38,27 @@ export default class Rectangle {
 
    set height(value){
       this._height = value;
+      this._halfHeight = value / 2;
+      this._eighthHeight = value / 8;
    }
 
    get height(){
       return this._height;
+   }
+
+   get halfWidth() {
+      return this._halfWidth;
+   }
+
+   get halfHeight() {
+      return this._halfHeight;
+   }
+
+   get eighthWidth() {
+      return this._eighthWidth;
+   }
+
+   get eighthHeight() {
+      return this._eighthHeight;
    }
 }
--- src/rectangle.spec.js
+++ src/rectangle.spec.js
@@ -31,4 +31,27 @@ describe('Rectangle Spec', () => {
       expect(rectangle.height).toBe(33);
       expect(rectangle2.height).toBe(43);
    });
+
+   it('creating rect will calculate 1/2 and 1/8th values of width and height', function() {
+      var rect = new Rectangle({x:0, y:0, width:160, height:160});
+
+      expect(rect.halfWidth).toBe(80);
+      expect(rect.eighthWidth).toBe(20);
+      expect(rect.halfHeight).toBe(80);
+      expect(rect.eighthHeight).toBe(20);
+   });
+
+   it('setting width will calculate 1/2 and 1/8th values of the width value', function() {
+      rectangle.width = 100;
+
+      expect(rectangle.halfWidth).toBe(50);
+      expect(rectangle.eighthWidth).toBe(12.5);
+   });
+
+   it('setting height will calculate 1/2 and 1/8th values of the height value', function() {
+      rectangle.height = 120;
+
+      expect(rectangle.halfHeight).toBe(60);
+      expect(rectangle.eighthHeight).toBe(15);
+   });
 });
