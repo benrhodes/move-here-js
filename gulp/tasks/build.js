76511f851dbbbf6f186bcda91702464bbe752da4
Ben Rhodes <b.rhodes@techsmith.com>
2016-02-24 20:15:33 -0500

    Bundle with rollup

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,75 +1,21 @@
 'use strict';
 
 var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var babel = require('babelify');
-var uglify = require('gulp-uglify');
+var rollup = require('gulp-rollup');
+var rename = require("gulp-rename");
+var babel = require("rollup-plugin-babel");
 
-var destFolder = './dist';
-var buildFolder = './build';
-
-function compile(params) {
-   params = params || {};
-   var bundler = watchify(browserify('./src/exports.js', { debug: true }).transform(babel));
-
-   function distBundle() {
-      bundler.bundle()
-         .on('end', function() {
-            console.log('-> done min ');
-            buildBundle(destFolder);
-         })
-         .on('error', function(err) { console.error(err); this.emit('end'); })
-         .pipe(source('move-here.min.js'))
-         .pipe(buffer())
-         .pipe(uglify())
-         .pipe(gulp.dest(destFolder));
-   }
-
-   function buildBundle(dest) {
-      dest = dest || buildFolder;
-      bundler.bundle()
-         .on('end', function() { console.log('-> done '); })
-         .on('error', function(err) { console.error(err); this.emit('end'); })
-         .pipe(source('move-here.js'))
-         .pipe(buffer())
-         .pipe(sourcemaps.init({ loadMaps: true }))
-         .pipe(sourcemaps.write('./'))
-         .pipe(gulp.dest(dest));
-   }
-
-   if (params.watch) {
-      bundler.on('update', function() {
-         console.log('-> bundling...');
-         buildBundle();
-      });
-   }
-
-   if(params.dist) {
-      distBundle();
-   } else {
-      buildBundle();
-   }
-}
-
-var dist = function() {
-   return compile({dist:true});
+var bundleTask = function(){
+   gulp.src('src/exports.js', {read: false})
+      .pipe(rollup({
+         sourceMap: true,
+         plugins: [
+            babel()
+         ]
+      }))
+      .pipe(rename("move-here.js"))
+      .pipe(gulp.dest('dist'));
 };
 
-var build = function() {
-   return compile();
-};
-
-var watch = function() {
-   return compile({watch:true});
-};
-
-gulp.task('watch', watch);
-gulp.task('dist', dist);
-gulp.task('build', build);
-module.exports = watch;
-module.exports = dist;
-module.exports = build;
+gulp.task('bundle', bundleTask);
+module.exports = bundleTask;
--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,7 @@
 
 var gulp = require('./gulp')([
    'webserver',
-   'jshint',
-   'cleandocs',
    'build'
 ]);
 
-var Server = require('karma').Server;
-var karma = function (done) {
-   new Server({
-      configFile: __dirname + '/karma.conf.js',
-      singleRun: true
-   }, done).start();
-};
-
-gulp.task('karma', karma);
-gulp.task('debugBuild', ['karma', 'build']);
-gulp.task('distBuild', ['karma', 'dist']);
-gulp.task('default', ['debugBuild']);
+gulp.task('default', ['build']);
