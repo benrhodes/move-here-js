45cc3a7e79599c00d29f534abbbfb588825c311f
Ben Rhodes <b.rhodes@techsmith.com>
2016-02-25 16:49:30 -0500

    Added karma gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,13 @@ var gulp = require('./gulp')([
    'build'
 ]);
 
+var Server = require('karma').Server;
+var karma = function (done) {
+   new Server({
+      configFile: __dirname + '/karma.conf.js',
+      singleRun: true
+   }, done).start();
+};
+
+gulp.task('karma', karma);
 gulp.task('default', ['build']);
